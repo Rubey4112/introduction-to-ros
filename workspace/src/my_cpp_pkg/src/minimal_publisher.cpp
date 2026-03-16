d47f5ef97a88adcda6c2e47df5d5ea1d511a846d
Toan Do <74166553+Rubey4112@users.noreply.github.com>
2026-03-15 20:23:05 -0400

    Coded minimal_publisher in C++

---
+++ workspace/src/my_cpp_pkg/src/minimal_publisher.cpp
@@ -0,0 +1,48 @@
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/string.hpp"
+
+class MinimalPublisher : public rclcpp::Node
+{
+
+public:
+  
+  MinimalPublisher() : Node{"minimal_publisher"}, counter_{0} // Call the parent constructor from the initilizer list 
+  {
+
+    publisher_ = this->create_publisher<example_interfaces::msg::String>("my_topic", 10);
+
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&MinimalPublisher::timer_callback, this));
+
+  }
+
+private:
+  // Member varibles
+  rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  size_t counter_;
+
+  // Private methods
+  void timer_callback() {
+    
+    auto msg = example_interfaces::msg::String();
+    msg.data = "Hello world: " + std::to_string(counter_);
+
+    publisher_->publish(msg);
+    RCLCPP_INFO(this->get_logger(), "Publishing: %s", msg.data.c_str());
+
+    counter_++;
+  }
+};
+
+// Entry point
+int main(int argc, char* argv[]) {
+  
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<MinimalPublisher>();
+  rclcpp::spin(node);
+
+  rclcpp::shutdown();
+
+  return 0;
+}
